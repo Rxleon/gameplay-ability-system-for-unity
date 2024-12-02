5e07092da7c123776bf2b8fc5517605780b0236b
Ex-Hard <390178965@qq.com>
2024-12-03 00:18:16 +0800

    [feat] 优化instant ge的apply过程

---
+++ Assets/GAS/RuntimeWithECS/GameplayEffect/Aspect/AspModifyBaseValue.cs
@@ -0,0 +1,61 @@
+using GAS.RuntimeWithECS.Attribute.Component;
+using GAS.RuntimeWithECS.AttributeSet.Component;
+using GAS.RuntimeWithECS.Core;
+using GAS.RuntimeWithECS.GameplayEffect.Component;
+using GAS.RuntimeWithECS.Modifier;
+using Unity.Entities;
+
+namespace GAS.RuntimeWithECS.GameplayEffect.Aspect
+{
+    public readonly partial struct AspModifyBaseValue : IAspect
+    {
+        public readonly Entity self;
+        private readonly RefRO<ComInUsage> _inUsage;
+        private readonly RefRO<ComValidEffect> _comValidEffect;
+        private readonly RefRO<ComInApplicationProgress> _inApplicationProgress;
+        private readonly DynamicBuffer<BuffEleModifier> _modifiers;
+
+        public void ModifyBaseValue()
+        {
+            // 排除掉Durational的GE类型
+            var isDurational = GASManager.EntityManager.HasComponent<ComDuration>(self);
+            if (isDurational) return;
+
+            var asc = _inUsage.ValueRO.Target;
+            var attrSets = GASManager.EntityManager.GetBuffer<AttributeSetBufferElement>(asc);
+            foreach (var mod in _modifiers)
+            {
+                var attrSetIndex = attrSets.IndexOfAttrSetCode(mod.AttrSetCode);
+                if (attrSetIndex == -1) continue;
+
+                var attrSet = attrSets[attrSetIndex];
+                var attributes = attrSet.Attributes;
+
+                var attrIndex = attributes.IndexOfAttrCode(mod.AttrCode);
+                if (attrIndex == -1) continue;
+
+                var data = attributes[attrIndex];
+                var oldValue = data.BaseValue;
+                var newValue = MmcHub.Calculate(self, mod, data.BaseValue);
+
+                // OnChangeBefore
+                // BaseValue 不做钳制，因为Max，Min是只针对Current Value
+                newValue = GASEventCenter.InvokeOnBaseValueChangeBefore(asc, mod.AttrSetCode, mod.AttrCode, newValue);
+
+                data.BaseValue = newValue;
+
+                // OnChangeAfter
+                if (newValue != oldValue)
+                {
+                    // BaseValue 改变，需要标记Dirty
+                    data.Dirty = true;
+                    GASManager.EntityManager.AddComponent<AttributeIsDirty>(asc);
+                    GASEventCenter.InvokeOnBaseValueChangeAfter(asc, mod.AttrSetCode, mod.AttrCode, oldValue, newValue);
+                }
+
+                attrSet.Attributes[attrIndex] = data;
+                attrSets[attrSetIndex] = attrSet;
+            }
+        }
+    }
+}
--- Assets/GAS/RuntimeWithECS/System/GameplayEffect/PhaseInstantEffect/SysInstantEffectModifyBaseValue.cs
+++ Assets/GAS/RuntimeWithECS/System/GameplayEffect/PhaseInstantEffect/SysInstantEffectModifyBaseValue.cs
@@ -3,6 +3,7 @@ using GAS.Runtime;
 using GAS.RuntimeWithECS.Attribute.Component;
 using GAS.RuntimeWithECS.AttributeSet.Component;
 using GAS.RuntimeWithECS.Core;
+using GAS.RuntimeWithECS.GameplayEffect.Aspect;
 using GAS.RuntimeWithECS.GameplayEffect.Component;
 using GAS.RuntimeWithECS.Modifier;
 using GAS.RuntimeWithECS.System.SystemGroup;
@@ -28,45 +29,11 @@ namespace GAS.RuntimeWithECS.System.GameplayEffect
         public void OnUpdate(ref SystemState state)
         {
             var ecb = new EntityCommandBuffer(Allocator.Temp);
-            
-            foreach (var (modifiers,_,_,_, geEntity) in SystemAPI
-                         .Query<DynamicBuffer<BuffEleModifier>,RefRO<ComInApplicationProgress>,RefRO<ComValidEffect>, RefRO<ComInUsage>>()
-                         .WithNone<ComDuration>()
-                         .WithEntityAccess())
+            foreach (var aspect in SystemAPI.Query<AspModifyBaseValue>())
             {
-                var asc = SystemAPI.GetComponentRO<ComInUsage>(geEntity).ValueRO.Target;
-                var attrSets = SystemAPI.GetBuffer<AttributeSetBufferElement>(asc);
-            
-                foreach (var mod in modifiers)
-                {
-                    int attrSetIndex = attrSets.IndexOfAttrSetCode(mod.AttrSetCode);
-                    if(attrSetIndex==-1) continue;
-                    
-                    var attrSet = attrSets[attrSetIndex];
-                    var attributes = attrSet.Attributes;
-            
-                    int attrIndex = attributes.IndexOfAttrCode(mod.AttrCode);
-                    if(attrIndex==-1) continue;
-                    
-                    var data = attributes[attrIndex];
-                    var baseValue = MmcHub.Calculate(geEntity, mod, data.BaseValue);
-                    // 加入base value 更新队列
-                    GasQueueCenter.AddBaseValueUpdateInfo(asc,mod.AttrSetCode,mod.AttrCode,baseValue);
-                    
-                    
-                    // data.TriggerCueEvent = true;
-                    // data.Dirty = true;
-                    // ecb.AddComponent<ComAttributeDirty>(geEntity);
-                    //
-                    // attrSet.Attributes[attrIndex] = data;
-                    // attrSets[attrSetIndex] = attrSet;
-                    
-                    // 应用完成的Instant GE，使其不合法
-                    ecb.SetComponentEnabled<ComInUsage>(geEntity, false);
-                }
-            
+                aspect.ModifyBaseValue();
+                ecb.RemoveComponent<ComValidEffect>(aspect.self);
             }
-            
             ecb.Playback(state.EntityManager);
             ecb.Dispose();
         }
@@ -76,67 +43,5 @@ namespace GAS.RuntimeWithECS.System.GameplayEffect
         {
 
         }
-        
-        
-        // public static void ApplyModFromInstantGameplayEffect(this Entity asc, Entity gameplayEffect)
-        // {
-        //     var attrSets = _entityManager.GetBuffer<AttributeSetBufferElement>(asc);
-        //     var modifiers = _entityManager.GetBuffer<BuffEleModifier>(gameplayEffect);
-        //     foreach (var mod in modifiers)
-        //     {
-        //         var magnitude = MmcHub.Calculate(gameplayEffect, mod);
-        //
-        //         var attrSetIndex = attrSets.IndexOfAttrSetCode(mod.AttrSetCode);
-        //         if (attrSetIndex == -1) continue;
-        //
-        //         var attrSet = attrSets[attrSetIndex];
-        //         var attributes = attrSet.Attributes;
-        //
-        //         var attrIndex = attributes.IndexOfAttrCode(mod.AttrCode);
-        //         if (attrIndex == -1) continue;
-        //
-        //         var attr = attributes[attrIndex];
-        //         var oldValue = attr.BaseValue;
-        //         var newValue = oldValue;
-        //         switch (mod.Operation)
-        //         {
-        //             case GEOperation.Add:
-        //                 newValue += magnitude;
-        //                 break;
-        //             case GEOperation.Minus:
-        //                 newValue -= magnitude;
-        //                 break;
-        //             case GEOperation.Multiply:
-        //                 newValue *= magnitude;
-        //                 break;
-        //             case GEOperation.Divide:
-        //                 newValue /= magnitude;
-        //                 break;
-        //             case GEOperation.Override:
-        //                 newValue = magnitude;
-        //                 break;
-        //             default:
-        //                 throw new ArgumentOutOfRangeException();
-        //         }
-        //
-        //         // OnChangeBefore
-        //         // BaseValue 不做钳制，因为Max，Min是只针对Current Value
-        //         newValue = GASEventCenter.InvokeOnBaseValueChangeBefore(asc, mod.AttrSetCode, mod.AttrCode, newValue);
-        //
-        //         attr.BaseValue = newValue;
-        //
-        //         // OnChangeAfter
-        //         if (newValue != oldValue)
-        //         {
-        //             // BaseValue 改变，需要标记Dirty
-        //             attr.Dirty = true;
-        //             GASManager.EntityManager.AddComponent<AttributeIsDirty>(asc);
-        //             GASEventCenter.InvokeOnBaseValueChangeAfter(asc, mod.AttrSetCode, mod.AttrCode, oldValue, newValue);
-        //         }
-        //
-        //         attrSet.Attributes[attrIndex] = attr;
-        //         attrSets[attrSetIndex] = attrSet;
-        //     }
-        // }
     }
 }
