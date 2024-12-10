0d07bfcaf0565abd5e6f0aaaa01ace2f9112a259
ex-hard <390178965@qq.com>
2024-12-10 20:25:45 +0800

    Update ComCueOnAdd.cs

--- Assets/GAS/RuntimeWithECS/GameplayEffect/Component/Static/ComCueOnAdd.cs
+++ Assets/GAS/RuntimeWithECS/GameplayEffect/Component/Static/ComCueOnAdd.cs
@@ -1,3 +1,6 @@
+using GAS.RuntimeWithECS.Core;
+using GAS.RuntimeWithECS.Cue;
+using GAS.RuntimeWithECS.Cue.Component;
 using Unity.Collections;
 using Unity.Entities;
 
@@ -6,5 +9,25 @@ namespace GAS.RuntimeWithECS.GameplayEffect.Component
     public struct ComCueOnAdd : IComponentData
     {
         public NativeArray<Entity> cues;
+    }
+    
+    public sealed class ConfComCueOnAdd:GameplayEffectComponentConfig
+    {
+        public CueInstant[] cues;
+        
+        public override void LoadToGameplayEffectEntity(Entity ge)
+        {
+            Entity[] entities = new Entity[cues.Length];
+            for (int i = 0; i < cues.Length; i++)
+            {
+                entities[i] = GASManager.EntityManager.CreateEntity();
+                GASManager.EntityManager.AddComponent<ComInstantCue>(entities[i]);
+                GASManager.EntityManager.SetComponentData(entities[i],new ComInstantCue(cues[i]));
+            }
+            _entityManager.AddComponentData(ge, new ComCueOnAdd
+            {
+                cues = new NativeArray<Entity>(entities, Allocator.Persistent)
+            });
+        }
     }
 }
