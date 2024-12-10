46ef0cb70aa68dcdc8feb93adf613058493d93b2
ex-hard <390178965@qq.com>
2024-12-10 20:55:01 +0800

    [feat] change

---
+++ Assets/GAS/RuntimeWithECS/System/SystemGroup/SysGroupDurationalEffect.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace GAS.RuntimeWithECS.System.SystemGroup
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateAfter(typeof(SysGroupInstantEffect))]
+    public partial class SysGroupDurationalEffect : ComponentSystemGroup
+    {
+    }
+}
